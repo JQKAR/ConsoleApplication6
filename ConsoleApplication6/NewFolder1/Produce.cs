00a5885bfc15cbdcbe2619f38fa31ee1bf355b28
RYU <RYU@JQKAR>
2017-02-13 13:17:30 -0500

    Add project files.

---
+++ ConsoleApplication6/NewFolder1/Produce.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleApplication6.Model
+{
+    class Produce // define custom classes
+    {
+        public Produce()
+        {
+
+        }// no-argument constructor
+
+        public Produce(string name, double weight, int quantity) //custom constructor
+        {
+            Name = name;
+            Weight = weight;
+            Quantity = quantity; // calling the property object and that implicilty is calling the set method
+        }
+
+        private string name; // this is a field with and  identifier known as name or name field
+        public string Name // the Name property
+        {
+            get { return name.ToUpper(); }
+            set { name = value; }
+        }
+
+        private double weight;
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Quantity { get; set; } // shorthand set and get declarations
+        public override string ToString()
+        {
+            return Name + "(" + Weight + "oz): " + Quantity;
+        }
+    }
+
+    class Fruit : Produce// Fruit is a subclass (derived class) of produce (base class)
+    {
+        public Fruit()
+        {
+
+        }
+
+        public Fruit(string name, double weight, int quantity) : base(name, weight, quantity)
+        {
+
+        }
+    }
+
+    class Vegetable : Produce
+    {
+        public Vegetable()
+        {
+
+        }
+
+        public Vegetable(string name, double weight, int quantity) : base(name, weight, quantity)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApplication6.Model;
+using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var produce = new List<object>(); // explicitly create the object 
+            var f1 = new Fruit("Apple ", 8.5, 3); // instant of a class and i'm instantiating it with Fruit
+            //f1.Serialnumber = 0001;
+            produce.Add(f1);
+
+            var f2 = new Fruit("Banana ", 4.5, 7);
+            //f2.Serialnumber = 0002;
+            produce.Add(f2);
+
+
+
+            produce.Add(new Vegetable("Carrot ", 4.1, 16)); // one statement
+            Console.WriteLine("There are: " + produce.Count + " items");
+
+            foreach (var item in produce)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
